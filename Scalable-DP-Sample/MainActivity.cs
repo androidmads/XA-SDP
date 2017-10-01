d7bd6a61c8a5ae360a8f9e0de7f1c07e0d6a2be7
androidmads <mushtaqat3gb@gmail.com>
2017-10-01 17:51:46 +0530

    Add project files.

---
+++ Scalable-DP-Sample/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Scalable_DP_Sample
+{
+    [Activity(Label = "Scalable DP Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
